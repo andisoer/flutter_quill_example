639dfc79daae0201c80ae0f4b3ea68c91b7fabc7
Andi Surya <andi502.as@gmail.com>
2023-06-22 17:28:38 +0700

    flutter_quill implementation

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' hide Text;
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final QuillController quillController = QuillController.basic();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Quill'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            QuillToolbar.basic(controller: quillController),
+            QuillEditor.basic(
+              controller: quillController,
+              readOnly: false,
+            ),
+          ],
+        ),
       ),
     );
   }
